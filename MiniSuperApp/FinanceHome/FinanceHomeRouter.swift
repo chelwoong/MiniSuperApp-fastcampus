e4b17ebb14a65d49eccc238a22028f82abc39e7c
woongs <woongs.rich@kakaocorp.com>
2021-10-30 15:38:19 +0900

    attach SuperPayDashboard

--- MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
+++ MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
@@ -1,19 +1,36 @@
 import ModernRIBs
 
-protocol FinanceHomeInteractable: Interactable {
-  var router: FinanceHomeRouting? { get set }
-  var listener: FinanceHomeListener? { get set }
+protocol FinanceHomeInteractable: Interactable, SuperPayDashboardListener {
+    var router: FinanceHomeRouting? { get set }
+    var listener: FinanceHomeListener? { get set }
 }
 
 protocol FinanceHomeViewControllable: ViewControllable {
-  // TODO: Declare methods the router invokes to manipulate the view hierarchy.
+    func addDashboard(_ view: ViewControllable)
 }
 
 final class FinanceHomeRouter: ViewableRouter<FinanceHomeInteractable, FinanceHomeViewControllable>, FinanceHomeRouting {
-  
-  // TODO: Constructor inject child builder protocols to allow building children.
-  override init(interactor: FinanceHomeInteractable, viewController: FinanceHomeViewControllable) {
-    super.init(interactor: interactor, viewController: viewController)
-    interactor.router = self
-  }
+    
+    private let superPayDashboardBuildable : SuperPayDashboardBuildable
+    private var superPayRouting: Routing?
+    
+    init(
+        interactor: FinanceHomeInteractable,
+        viewController: FinanceHomeViewControllable,
+        superPayDashboardBuildable: SuperPayDashboardBuildable
+    ) {
+        self.superPayDashboardBuildable = superPayDashboardBuildable
+        super.init(interactor: interactor, viewController: viewController)
+        interactor.router = self
+    }
+    
+    func attachSuperPayDashboard() {
+        guard self.superPayRouting == nil else { return }
+        
+        let router = self.superPayDashboardBuildable.build(withListener: self.interactor)
+        let dashboard = router.viewControllable
+        self.viewController.addDashboard(dashboard)
+        self.superPayRouting = router
+        self.attachChild(router)
+    }
 }
--- MiniSuperApp/FinanceHome/FinanceHomeViewController.swift
+++ MiniSuperApp/FinanceHome/FinanceHomeViewController.swift
@@ -2,42 +2,60 @@ import ModernRIBs
 import UIKit
 
 protocol FinanceHomePresentableListener: AnyObject {
-  // TODO: Declare properties and methods that the view controller can invoke to perform
-  // business logic, such as signIn(). This protocol is implemented by the corresponding
-  // interactor class.
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
 }
 
 final class FinanceHomeViewController: UIViewController, FinanceHomePresentable, FinanceHomeViewControllable {
-  
-  weak var listener: FinanceHomePresentableListener?
-  
-  init() {
-    super.init(nibName: nil, bundle: nil)
     
-    setupViews()
-  }
-  
-  required init?(coder: NSCoder) {
-    super.init(coder: coder)
+    weak var listener: FinanceHomePresentableListener?
     
-    setupViews()
-  }
-  
-  private let label: UILabel = {
-    let label = UILabel()
-    label.translatesAutoresizingMaskIntoConstraints = false
-    return label
-  }()
-  
-  func setupViews() {
-    title = "슈퍼페이"
-    tabBarItem = UITabBarItem(title: "슈퍼페이", image: UIImage(systemName: "creditcard"), selectedImage: UIImage(systemName: "creditcard.fill"))
-    label.text = "Finance Home"
-    view.backgroundColor = .systemBlue
-    view.addSubview(label)
-    NSLayoutConstraint.activate([
-      label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-    ])
-  }
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 4
+        return stackView
+    }()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupViews()
+    }
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupViews() {
+        title = "슈퍼페이"
+        tabBarItem = UITabBarItem(title: "슈퍼페이", image: UIImage(systemName: "creditcard"), selectedImage: UIImage(systemName: "creditcard.fill"))
+        self.view.backgroundColor = .white
+        self.view.addSubview(self.stackView)
+        
+        NSLayoutConstraint.activate([
+            self.stackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.stackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.stackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+    }
+    
+    func addDashboard(_ view: ViewControllable) {
+        let viewController = view.uiviewController
+        addChild(viewController)
+        stackView.addSubview(viewController.view)
+        viewController.didMove(toParent: self)
+    }
 }
