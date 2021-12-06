39ff3120010eca123994b07a4cd79b6b15699b09
woongs <woongs.rich@kakaocorp.com>
2021-12-06 17:06:06 +0900

    RIBsTestSupport 패키지 추가

--- Platform/Package.swift
+++ Platform/Package.swift
@@ -16,6 +16,10 @@ let package = Package(
             targets: ["RIBsUtil"]
         ),
         .library(
+            name: "RIBsTestSupport",
+            targets: ["RIBsTestSupport"]
+        ),
+        .library(
             name: "SuperUI",
             targets: ["SuperUI"]
         ),
